ce924a1f9420c285eea8c7038ce0d0f6e89d582e
242566 Wlodarski Bartlomiej <242566@edu.p.lodz.pl>
2024-11-17 17:37:41 +0100

    [+] Added camera control using mouse

---
+++ Assets/Scripts/MouseCamera.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class MouseCamera : MonoBehaviour
+{
+    public Vector2 turn;
+    public float sensitivity = .5f;
+    public Vector3 deltaMove;
+    public float speed = 1;
+    public GameObject mover;
+
+    [Header("Camera bounds")]
+    public float xMax = 20;
+    public float xMin = -20;
+    public float yMax = 25;
+    public float yMin = 0.5f;
+    public float zMax = 35;
+    public float zMin = -20;
+
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            turn.x += Input.GetAxis("Mouse X") * sensitivity;
+            turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        }
+
+        deltaMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+        transform.Translate(deltaMove);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, xMin, xMax),
+            Mathf.Clamp(transform.position.y, yMin, yMax),
+            Mathf.Clamp(transform.position.z, zMin, zMax)
+            );
+    }
+}
